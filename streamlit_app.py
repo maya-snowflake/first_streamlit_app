25b45c32e2c6174d7d71cc2ef13c3b26298a0094
maya-snowflake <114959030+maya-snowflake@users.noreply.github.com>
2022-10-11 15:30:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,8 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:
     back_from_fucntion = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_fucntion)
+    
 except URLError as e:
   streamlit.error()
   
